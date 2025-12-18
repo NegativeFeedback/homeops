2dd20a3ac891b953e1cde1f7615429e7b26395d3
negativefeedback <negativefeedback@pm.me>
2025-12-18 16:02:51 -0600

    testing terrakube and adding tofu state to the kms mount in vault

--- terraform/homeops/transit.tf
+++ terraform/homeops/transit.tf
@@ -3,3 +3,10 @@ resource "vault_mount" "kms" {
   type = "transit"
 }
 
+resource "vault_transit_secret_backend_key" "tofu_state" {
+  backend = vault_mount.kms.path
+  name    = "tofu_state"
+  type               = "aes256-gcm96"
+  deletion_allowed   = false
+  exportable         = false
+}
